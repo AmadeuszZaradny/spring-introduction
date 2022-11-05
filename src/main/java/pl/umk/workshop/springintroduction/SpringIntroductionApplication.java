b3159435557957cf8e8b6532dbc59ccf981d89e0
Amadeusz <amadeusz.zaradny@allegro.pl>
2022-11-05 19:28:25 +0100

    initial commit

---
+++ src/main/java/pl/umk/workshop/springintroduction/SpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package pl.umk.workshop.springintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/umk/workshop/springintroduction/SpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umk.workshop.springintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
